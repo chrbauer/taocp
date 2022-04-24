59e54f5f6d3f54419d6b4926a51132dd2e44d187
Christoph Bauer <mail@christoph-bauer.net>
2022-04-24 10:22:29 +0200

    refactor

--- haskell/src/TAOCP_1A/Log.hs
+++ haskell/src/TAOCP_1A/Log.hs
@@ -1,19 +1,21 @@
-module TAOCP_1A.Log where
+module TAOCP_1A.Log
+  (log, log10, ln, lg)
+where
 
 import Prelude hiding (log)
 import Data.Array
 
-log :: Double -> Double -> Double
-log b x = a + (num 0.5 $ take 52 $ bits b x0)
+logG :: (Floating a, Ord a) => a -> a -> a
+logG b x = a + (num 0.5 $ take 52 $ bits b x0)
   where (a, x0) =  split b x 0 
 
-split :: Double -> Double -> Double -> (Double, Double)
+split :: (Floating a, Ord a) => a -> a -> a -> (a, a)
 split b x u 
   | x < b     = (u, x)
   | otherwise = split b (x/b) (1+u)
           
 
-bits :: Double -> Double -> [Bool]
+bits :: (Floating a, Ord a) => a -> a -> [Bool]
 bits b x
   | x2 > b = True : bits b (x2/b)
   | otherwise = False : bits b x2
@@ -24,16 +26,30 @@ num _ [] = 0
 num d (False:t) = num (d/2) t
 num d (True:t) = d+num (d/2) t
 
-
-l :: Double -> Double -> Double
-l b x 
-  | x < 1 || x >= 2 = error "x"
-  | otherwise       = go x 0 (shiftR x 1) 1
-  where go x y z k
+-- we want to keep tab for log10, lg and ln 
+{-# INLINABLE log #-}
+log :: (Floating a, Ord a) => a -> a -> a
+log b x 
+  | x <= 0 = error "x <= 0"
+  | x < 1 = -(log b (1/x))
+    --let eps = 1 - x in 
+    --  feynman (1-x) (b ** (1-eps)) 1
+  | x >= 2 = 1 + log b (x/b)
+  | otherwise       = briggs x 0 (shiftR x 1) (1 :: Int)
+  where briggs x y z k
           | x == 1.0 || k > 53 = y
-          | x - z < 1 = go x y (shiftR z 1) (k+1)
-          | otherwise = let x' = x-z in go x' (y + tab ! k) (shiftR x' k) k
+          | x - z < 1 = briggs x y (shiftR z 1) (k+1)
+          | otherwise = let x' = x-z in briggs x' (y + tab ! k) (shiftR x' k) k
         shiftR x k = x / (2.0 ^ k)
-        tab     = array (1, 128) [(k, log b (2^k/(2^k-1))) | k <- [1..53]]
+        tab     = array (1, 128) [(k, logG b (2^k/(2^k-1))) | k <- [1..53]]
+        -- TODO: find the error
+        feynman x y k 
+          | x == 1 || k > 50 = y
+          | x < tab ! k      = feynman x y (k+1)
+          | otherwise        = feynman (x - tab ! k) (y - shiftR y k) k
+        
 
-lg = l 2
+e = exp 1
+log10 = log 10
+lg = log 2
+ln = log e
